a0ea53dc4d2ad1ce555b644e276411592c511de3
Chanchan Aueaprasroed <AUEAPRASROED_C@silpakorn.edu>
2026-09-10 04:36:12 +0000

    data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import { movies } from './data/data';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
